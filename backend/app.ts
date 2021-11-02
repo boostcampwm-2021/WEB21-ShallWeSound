575a5e3f5aad29865c5db005895fd1b6529a696b
kbpark9898 <kbpark9898@gmail.com>
2021-11-02 15:18:36 +0900

    [#17] feat: 백앤드에 소켓 추가 및 소켓 서버 생성

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,26 @@
 import express from 'express';
-
+import {Server} from 'socket.io';
+import http from 'http';
 const app: express.Application = express();
-
+const server:http.Server = http.createServer(app);
+const io = new Server(server);
 app.get('/', (req, res) => {
   res.send('정상 작동합니다.');
 });
+interface userList{
+  [socketid:number] : number;
+}
+const userHash:userList={}
+let userNum:number = 0;
+io.on('connection', (socket)=>{
+  console.log('new user connected');
+  socket.broadcast.emit('enterRoom', 'new user connected');
+  socket.on('disconnect', ()=>{
+    socket.broadcast.emit('leaveRoom', 'user disconnected');
+  })
+  socket.on('chatMessage', (message:string)=>{
+    socket.broadcast.emit('chatMessage', message);
+  })
+})
 
-app.listen(3000, () => {
-  console.log('작동중');
-});
+server.listen(5000, () => console.log("App listening on port 5000..."));
