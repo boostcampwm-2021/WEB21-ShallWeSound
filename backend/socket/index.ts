76ae94e13dc1681d73e8f5bb1f28e81583369d76
seongjunme <72444675+seongjunme@users.noreply.github.com>
2021-11-02 17:14:44 +0900

    [#4] feat: socket 에서 관리하는 playList 배열 생성
    - 임시로 노래 정보 삽입
    - 추후에 playList 관련 메소드 제작

--- backend/socket/index.ts
+++ backend/socket/index.ts
@@ -1,12 +1,9 @@
 import { Server } from 'socket.io';
 import http from 'http';
+import music from './music';
 
 export default (server: http.Server) => {
   const io = new Server(server, { cors: { origin: '*' } });
 
-  io.on('connection', socket => {
-    console.log('socket connection');
-  });
-  // userSpace(io, sessionMiddleware);
-  // multiSpace(io);
+  music(io);
 };
