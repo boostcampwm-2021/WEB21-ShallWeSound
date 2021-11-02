c46e4d3236166f1bd472e378be9c4c983be00936
kbpark9898 <kbpark9898@gmail.com>
2021-11-02 16:55:43 +0900

    [#17] feat: 프론트앤드 / 백앤드 프록시 추가

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import {Server} from 'socket.io';
 import http from 'http';
+import cors from 'cors';
 const app: express.Application = express();
+app.use(cors);
 const server:http.Server = http.createServer(app);
 const io = new Server(server);
 app.get('/', (req, res) => {
