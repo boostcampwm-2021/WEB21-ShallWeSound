78c66e9f77448b1ec7492188eaec8323c2c5e8de
seongjunme <72444675+seongjunme@users.noreply.github.com>
2021-11-03 18:56:16 +0900

    [#4] feat: 플레이 리스트에서 다음 곡을 불러오는 함수 생성
    - 음악 공유 스트리밍에서 다음 재생할 곡 정보를 제공

--- backend/socket/music.ts
+++ backend/socket/music.ts
@@ -1,24 +1,56 @@
 import { Server } from 'socket.io';
+import type { Music } from '../types';
 
-interface Music {
-  name: string;
+interface PlayList {
+  playlist: Music[];
+  getPlayList: () => Music[];
+  getNextMusic: () => Music;
 }
 
-export default (io: Server) => {
-  const namespace = io.of('/music');
+const PlayList: PlayList = {
+  playlist: [
+    {
+      name: 'Savage',
+      src: 'http://localhost:3000/audio/Aespa_Savage.mp3',
+      isPlayed: false,
+    },
+    {
+      name: 'Psycho',
+      src: 'http://localhost:3000/audio/RedVelvet_Psycho.mp3',
+      isPlayed: false,
+    },
+    {
+      name: 'Blueming',
+      src: 'http://localhost:3000/audio/IU_Blueming.mp3',
+      isPlayed: false,
+    },
+  ],
+  getPlayList() {
+    return this.playlist;
+  },
+  getNextMusic() {
+    const now = this.playlist.find(music => music.isPlayed);
+    if (!now) {
+      this.playlist[0].isPlayed = true;
+      return this.playlist[0];
+    }
 
-  const playList: Music[] = [
-    {
-      name: '좋은 날',
-    },
-    {
-      name: 'savage',
-    },
-  ];
+    const next = this.playlist[(this.playlist.indexOf(now) + 1) % this.playlist.length];
+    now.isPlayed = false;
+    next.isPlayed = true;
+
+    return next;
+  },
+};
+
+const musicSocket = (io: Server) => {
+  const namespace = io.of('/music');
 
   namespace.on('connection', socket => {
     console.log(socket.id + ' connected');
 
-    socket.emit('init', playList);
+    socket.emit('init', PlayList.getPlayList());
   });
 };
+
+export { PlayList, musicSocket };
