a75c49ccfdb22d1c54054a3d04ff408b81688db1
seongjunme <72444675+seongjunme@users.noreply.github.com>
2021-11-03 18:56:16 +0900

    [#4] feat: 플레이 리스트에서 다음 곡을 불러오는 함수 생성
    - 음악 공유 스트리밍에서 다음 재생할 곡 정보를 제공

--- backend/socket/index.ts
+++ backend/socket/index.ts
@@ -1,9 +1,9 @@
 import { Server } from 'socket.io';
 import http from 'http';
-import music from './music';
+import { musicSocket } from './music';
 
 export default (server: http.Server) => {
   const io = new Server(server, { cors: { origin: '*' } });
 
-  music(io);
+  musicSocket(io);
 };
