05f0490385f930326976fb3e111e15c9e5ff84c9
이홍덕 <doggydeok2@naver.com>
2021-11-03 18:33:37 +0900

    [#9] feat: svg icon 추가 및 music index 전환 기능 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,50 @@
+import React, { useState } from 'react';
 import MusicPlayer from './components/MusicPlayer';
 
 function App() {
+  const [musicList, setMusicList] = useState([
+    {
+      name: "Harley Bird - Home",
+      singer: "Jordan Schor",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Ikson Anywhere – Ikson",
+      singer: "Audio Library",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Beauz & Jvna - Crazy",
+      singer: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Hardwind - Want Me",
+      singer: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      singer: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      singer: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+  ])
+
   return (
-    <MusicPlayer></MusicPlayer>
+    <>
+      <MusicPlayer musicList={musicList}></MusicPlayer>
+    </>
   );
 }
 
--- frontend/src/components/MusicPlayer.tsx
+++ frontend/src/components/MusicPlayer.tsx
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { nodeModuleNameResolver } from "typescript";
 import './MusicPlayer.scss'
 
-function MusicPlayer({ }) {
+function Title({ name="Test", singer="Singer" }) {
+  return (
+    <div className="musicplayer-title-area">
+      {/* <span>Daft Punk - Harder, Better, Faster, Stronger</span> */}
+      <span className="musicplayer-title">{name}</span>
+      <span className="musicplayer-subtitle">{singer}</span>
+    </div>
+  )
+}
+
+function PrevMusic ({ onClick } : { onClick: any }) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={onClick}>
+      <path d="M9 17L1 9L9 1" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
+
+function NextMusic ({ onClick } : { onClick: any }) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={onClick}>
+      <path d="M8 20L16 12L8 4" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
+
+function MusicPlayer({ musicList } : { musicList: any }) {
+  const [musicIndex, setmusicIndex] = useState(0);
+  const [musicInfo, setMusicInfo] = useState({
+    'name': 'noname',
+    'singer': 'noname'
+  });
+
+  function goPrevMusic() {
+    setmusicIndex((musicIndex - 1 + musicList.length) % musicList.length)
+  }
+  
+  function goNextMusic() {
+    setmusicIndex((musicIndex + 1) % musicList.length)
+  }
+
+  useEffect(() => {
+    setMusicInfo({
+      ...musicInfo,
+      'name': musicList[musicIndex]['name'],
+      'singer': musicList[musicIndex]['singer'],
+    })
+  }, [musicIndex])
+
   return (
     <>
       <div className="musicplayer">
-        <div className="musicplayer-title">제 목</div>
+        <Title name={musicInfo['name']} singer={musicInfo['singer']} />
         <div className="musicplayer-body">
-          <p>&lt;</p>
-          <div className="musicplayer-cover"></div>
-          <p>&gt;</p>
+          <PrevMusic onClick={goPrevMusic} />
+          <div className="musicplayer-cover">
+            <div className="cover-hover">
+              <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#FFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
+            </div>
+          </div>
+          <NextMusic onClick={goNextMusic} />
         </div>
-        <div className="musicplayer-timer">00:00 / 04:22</div>
-        <progress></progress>
+        <div className="musicplayer-timer">
+          <span className="current-time">00:00</span>
+          <span className="max-duration">04:22</span>
+        </div>
+        <div className="progress">
+          <div className="progress-bar">
+            <audio src=""></audio>
+          </div>
+        </div>
       </div>
     </>
   );
