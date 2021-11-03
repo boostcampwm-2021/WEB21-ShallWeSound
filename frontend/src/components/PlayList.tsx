13459819d963f1ff997f4f235ac3d3034ae7b8c7
seongjunme <72444675+seongjunme@users.noreply.github.com>
2021-11-03 22:13:59 +0900

    [#4] feat: 플레이 리스트 목록 CSS 작업

--- frontend/src/components/PlayList.tsx
+++ frontend/src/components/PlayList.tsx
@@ -1,31 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
 import type { Music } from '../types';
+import PlayListItem from './PlayListItem';
 
 type Props = {
   playList: Music[];
 };
 
 const PlayList = ({ playList }: Props) => {
-  console.log(playList);
-  playList.map(music => console.log(music.name));
   return (
-    <StyledDiv>
-      {playList.map((music: Music, i: number) => (
-        <div key={i}>{music.name}</div>
-      ))}
-    </StyledDiv>
+    <Container>
+      <Title>P L A Y &nbsp; L I S T</Title>
+      <Wrapper>
+        {playList.map((music: Music, i: number) => (
+          <PlayListItem key={i} name={music.name}></PlayListItem>
+        ))}
+      </Wrapper>
+      <AddButton>+</AddButton>
+    </Container>
   );
 };
 
-const StyledDiv = styled.div`
-  background-color: #d3d3d3;
-  border: 1px solid #000000;
-  box-shadow: rgb(0 0 0 / 20%) 0px 10px 25px;
-  width: 300px;
-  height: 800px;
-  /* margin: 48px; */
-  /* float: right; */
+const Container = styled.div`
+  background: linear-gradient(to top right, blue, pink);
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 10px 25px;
+  width: 250px;
+  height: 500px;
+  margin: 48px;
+  float: right;
+  position: relative;
+`;
+
+const Title = styled.div`
+  color: #ffffff;
+  font-weight: 500;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30px;
+  margin: 10px 0px;
+`;
+
+const Wrapper = styled.div`
+  height: 80%;
+  overflow: auto;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+`;
+
+const AddButton = styled.button`
+  border-radius: 50%;
+  border: 1px solid #ffffff;
+  padding: 0;
+  width: 45px;
+  height: 45px;
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: rgb(0 0 0 / 70%) 0px 10px 25px;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+    background-color: transparent;
+  }
 `;
 
 export default PlayList;
