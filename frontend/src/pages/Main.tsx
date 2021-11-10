64443f51a7f9064dec46da9381d0865c17984193
jsl0149 <jsl0149@naver.com>
2021-11-10 16:01:46 +0900

    [#59] feat : 방 디자인 수정

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -2,13 +2,18 @@ import { listenerCount } from 'process';
 import React, { useState, useContext, useEffect, useRef, EventHandler } from 'react';
 import { Socket } from 'socket.io-client';
 import { useSocket } from '../context/MyContext';
+import './Main.scss';
 
 import { Room } from './Room';
 
 export const MainPage = ({ history }: { history: any }) => {
   const socket: Socket = useSocket()!;
 
-  const [roomList, setRoomList] = useState(['최고다 아이유', '최고다 이순신', '최고다 김윈터']);
+  const [roomList, setRoomList] = useState([
+    '$$최고다 아이유 너만 오면고$$',
+    '$$최고다 이순신 입장시 불멸!$$',
+    '$$최고다 김윈터 ㄷㄷㄷㄷㄷㄷ$$',
+  ]);
 
   const fff = (e: React.MouseEvent<HTMLElement>) => {
     socket.emit('joinRoom', e.currentTarget.innerHTML);
@@ -29,11 +34,13 @@ export const MainPage = ({ history }: { history: any }) => {
   return (
     <>
       <div>메인 인데요</div>
-      {roomList.map(val => (
-        <div className={'room'} onClick={fff}>
-          {val}
-        </div>
-      ))}
+      <div className={'roomList'}>
+        {roomList.map(val => (
+          <div className={'room'} onClick={fff}>
+            {val}
+          </div>
+        ))}
+      </div>
       <input type={'button'} value={'방 추가'} onClick={리액트너무재밌는데}></input>
     </>
   );
