ab874f8652d6dd2b25bd3ca610853c1af3035890
kbpark9898 <kbpark9898@gmail.com>
2021-11-10 15:19:24 +0900

    [#47] feat: 음악 파일이 서버(디스크)를 거치지 않고 바로 오브젝트 스토리지로 올라가도록 구현

--- backend/routes/upload.ts
+++ backend/routes/upload.ts
@@ -2,21 +2,25 @@ import express from 'express';
 import multer from 'multer';
 import * as AWS from 'aws-sdk';
 import * as fs from 'fs';
-require('dotenv').config({path:'../config/.env'});
+import {Readable} from 'stream';
+import dotenv from 'dotenv';
+dotenv.config({path:'config/.env'});
 const upload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, 'uploads/');
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname);
-        }
-    }),
+    // storage: multer.diskStorage({
+    //     destination: function (req, file, cb) {
+    //         cb(null, 'uploads/');
+    //     },
+    //     filename: function (req, file, cb) {
+    //         cb(null, file.originalname);
+    //     }
+    // }),
+    storage: multer.memoryStorage()
 });
 const router = express.Router();
 const region = 'kr-standard';
 const access_key = `${process.env.ACCESS_KEY}`;
 const secret_key = `${process.env.SECRET_KEY}`;
+console.log(access_key, secret_key)
 const S3 = new AWS.S3({
     endpoint:'https://kr.object.ncloudstorage.com',
     region,
@@ -28,7 +32,9 @@ const S3 = new AWS.S3({
 
 router.post('/', upload.single('userFile'),(req, res)=>{
     const bucket_name = 'sws';
-    const object_name = `${req.file?.filename}`;
+    const object_name = `${req.file?.originalname}`;
+    // const object_name = `${req.file?.filename}`;
+    console.log(req.file)
     let options = {
         partSize: 5 * 1024 * 1024
     };
@@ -37,9 +43,10 @@ router.post('/', upload.single('userFile'),(req, res)=>{
         await S3.upload({
             Bucket: bucket_name,
             Key: object_name,
-            Body: fs.createReadStream(`./uploads/${req.file?.filename}`)
+            // Body: fs.createReadStream(`./uploads/${req.file?.filename}`)
+            Body: Readable.from(req.file!.buffer)
         }, options).promise();
-        await fs.unlink(`./uploads/${req.file?.filename}`, err=>{});
+        // await fs.unlink(`./uploads/${req.file?.filename}`, err=>{});
     })();
     res.send(200);
 })
