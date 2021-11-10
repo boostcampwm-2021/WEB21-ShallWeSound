235b528bf6c9e54a2a7c0cbee6aa41c942522673
kbpark9898 <kbpark9898@gmail.com>
2021-11-10 15:21:49 +0900

    [#47] feat: DB 연결을 위한 파일(모듈) 추가

---
+++ backend/config/db.ts
@@ -0,0 +1,20 @@
+import mysql from 'mysql';
+const db_info:mysql.ConnectionConfig = {
+    host: `${process.env.DB_HOST}`,
+    port: 3306,
+    user: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_DATABASE}`
+}
+const connection = mysql.createConnection(db_info);
+module.exports = {
+    db: function () {
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn:mysql.Connection) {
+        conn.connect(function(err) {
+            if(err) console.error('mysql connection error : ' + err);
+            else console.log('mysql is connected successfully!');
+        });
+    }
+}
