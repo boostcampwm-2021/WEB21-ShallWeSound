e1d5faed68ee2dfc628c7f27cd05e5255a682bb2
이홍덕 <doggydeok2@naver.com>
2021-11-11 10:35:54 +0900

    [#55] feat: 방 생성 Dialog 입력 폼 구현

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,5 +1,6 @@
 import { listenerCount } from 'process';
 import React, { useState, useContext, useEffect, useRef, EventHandler } from 'react';
+import styled, { css, keyframes } from 'styled-components';
 import { Socket } from 'socket.io-client';
 import { useSocket } from '../context/MyContext';
 import '../stylesheets/main.scss';
@@ -13,6 +14,13 @@ export const MainPage = ({ history }: { history: any }) => {
     '$$최고다 이순신 입장시 불멸!$$',
     '$$최고다 김윈터 ㄷㄷㄷㄷㄷㄷ$$',
   ]);
+  const [visible, setVisible] = useState(false);
+  const [appear, setAppear] = useState(false);
+  const [dialogInput, setDialogInput] = useState({
+    name: "",
+    description: "",
+  })
+
 
   const fff = (e: React.MouseEvent<HTMLElement>) => {
     socket.emit('joinRoom', e.currentTarget.innerHTML);
@@ -20,9 +28,9 @@ export const MainPage = ({ history }: { history: any }) => {
     history.push('/room');
   };
 
-  const 리액트너무재밌는데 = () => {
-    setRoomList([...roomList, '방을 추가해보자']);
-  };
+  // const 리액트너무재밌는데 = () => {
+  //   setRoomList([...roomList, '방을 추가해보자']);
+  // };
 
   useEffect(() => {
     socket.on('joinRoomClient', data => {
@@ -30,9 +38,56 @@ export const MainPage = ({ history }: { history: any }) => {
     });
   });
 
+  function changeDialogRoomName(e: React.BaseSyntheticEvent) {
+    setDialogInput({
+      ...dialogInput,
+      name: e.target.value
+    })
+  }
+
+  function changeDialogRoomDescription(e: React.BaseSyntheticEvent) {
+    setDialogInput({
+      ...dialogInput,
+      description: e.target.value
+    })
+  }
+
+  function toggleCreateRoomDialog() {
+    setVisible(!visible);
+  }
+
+  function createRoom() {
+    if (dialogInput.name && dialogInput.description) {
+      fetch('http://localhost:3000/api/room') // session 쓸때 credentials : 'include' 설정해주기
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.list);
+        console.log('여기로 오나?');
+      });
+    } else {
+      // error
+    }
+  }
+
   return (
     <>
-      <div>메인 인데요</div>
+      {visible && 
+        <div className="dark-background">
+          <div className="dialog">
+            <p>방 생성</p>
+            <form className="input-wrap" action="submit">
+              <label htmlFor="room-id">방 제목</label>
+              <input type="text" id="room-id" onChange={changeDialogRoomName}/>
+              <label htmlFor="room-detail">방 설명</label>
+              <input type="text" id="room-detail" onChange={changeDialogRoomDescription}/>
+            </form>
+            <div className="button-wrap">
+              <div className="button" onClick={createRoom}>생성</div>
+              <div className="button" onClick={toggleCreateRoomDialog}>취소</div>
+            </div>
+          </div>
+        </div>
+      }
       <div className={'roomList'}>
         {roomList.map(val => (
           <div className={'room'} onClick={fff}>
@@ -40,7 +95,7 @@ export const MainPage = ({ history }: { history: any }) => {
           </div>
         ))}
       </div>
-      <input type={'button'} value={'방 추가'} onClick={리액트너무재밌는데}></input>
+      <input type={'button'} value={'방 추가'} onClick={toggleCreateRoomDialog}></input>
     </>
   );
 };
