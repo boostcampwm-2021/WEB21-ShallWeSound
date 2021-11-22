4c7889095c77efdb313b6e8ced4f5ab28ccacc65
jsl0149 <jsl0149@naver.com>
2021-11-22 10:38:20 +0900

    [#89] feat : 검색 결과 페이지 구현

--- backend/routes/api/result.ts
+++ backend/routes/api/result.ts
@@ -1,20 +1,16 @@
 import express from 'express';
 import MusicService from '../../services/music';
-
-interface aaa {
-  name: string;
-}
+import { musicTable } from '../../types';
 
 const router = express.Router();
 
 router.get('/', async (req: express.Request, res: express.Response, next) => {
-  const result = await MusicService.search(`${req.query.keyword}`);
+  const result: musicTable[] = await MusicService.search(`${req.query.keyword}`);
+  const musicList = result.map(val => {
+    return { name: val.name, singer: val.singer, thumbnail: val.thumbnail };
+  });
 
-  // const musicList = result.map(val => {
-  //   return { name: val.name };
-  // });
-
-  res.json({ test: result });
+  res.json({ list: musicList });
 });
 
 export default router;
--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -14,12 +14,21 @@ const ResultPage = () => {
     const keyword = window.location.pathname.match(/[^/]+/gm)![1];
     fetch(`${config.localhost}/api/result?keyword=${keyword}`)
       .then(res => res.json())
-      .then(data => console.log(data.test[0].name));
-  });
+      .then(data => setResultList(data.list));
+  }, []);
 
   return (
     <>
       <div>여긴 결과페이지다.</div>
+      {resultList.map(val => {
+        return (
+          <div>
+            {val.name}
+            {val.singer}
+            <img src={val.thumbnail}></img>
+          </div>
+        );
+      })}
     </>
   );
 };
