9b12fee9005f3b14b9d7c914d91f163c74d7c720
이홍덕 <doggydeok2@naver.com>
2021-11-25 16:51:40 +0900

    [#106] 검색 결과 내 뮤직플레이어 동작 개선

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -6,13 +6,21 @@ function SearchedMusicPlayer ({ path, isPlay } : { path: string, isPlay: boolean
   const musicControl = useRef<HTMLVideoElement | null>(null);
   const musicProgress = useRef<HTMLInputElement>(null);
   const volumeProgress = useRef<HTMLInputElement>(null);
-  const [backupMusicVolume, setBackupMusicVolume] = useState(0);
+  const [musicPlayerState, setMusicPlayerState] = useState({
+    currentTime: 0,
+    volume: 0.5,
+    backupVolume: 0.5
+  });
 
   function updateCurrentTime() {
     const playingMusic = musicControl.current;
     const playingMusicProgress = musicProgress.current;
     if (playingMusic && playingMusicProgress) {
       playingMusicProgress.value = playingMusic.currentTime.toString();
+      setMusicPlayerState({
+        ...musicPlayerState,
+        currentTime: playingMusic.currentTime
+      });
       playingMusicProgress.style.backgroundSize = (playingMusic.currentTime * 100) / playingMusic.duration + '% 100%';
     }
   }
@@ -47,55 +55,72 @@ function SearchedMusicPlayer ({ path, isPlay } : { path: string, isPlay: boolean
     const musicVolume = volumeProgress.current;
     if (playingMusic && musicVolume) {
       if (playingMusic.volume > 0) {
-        setBackupMusicVolume(playingMusic.volume);
+        setMusicPlayerState({
+          ...musicPlayerState,
+          backupVolume: playingMusic.volume
+        });
         musicVolume.value = '0';
         playingMusic.volume = 0;
         musicVolume.style.backgroundSize = playingMusic.volume * 100 + '% 100%';
       } else {
-        musicVolume.value = (backupMusicVolume * 100).toString();
-        playingMusic.volume = backupMusicVolume;
+        musicVolume.value = (musicPlayerState.backupVolume * 100).toString();
+        playingMusic.volume = musicPlayerState.backupVolume;
         musicVolume.style.backgroundSize = playingMusic.volume * 100 + '% 100%';
       }
     }
   }
 
+  useEffect (() => {
+    setMusicPlayerState({
+      ...musicPlayerState,
+      currentTime: 0,
+      volume: 0.5,
+    })
+    toggleVolume();
+    toggleVolume();
+  }, []);
+
   return (
-    <div className="searched-musicplayer">
-      <video id="video" src={path} autoPlay ref={musicControl} onTimeUpdate={updateCurrentTime}/> 
-      <div className="musicplayer-timer">
-        <span className="current-time">{changeFormatToTime(musicControl.current?.currentTime || 0)}</span>
-        <span className="max-duration">{changeFormatToTime(musicControl.current?.duration || 0)}</span>
-      </div>
-      <input
-        className="input-range"
-        name="musicplayer-progress"
-        ref={musicProgress}
-        type="range"
-        min="0"
-        max={musicControl.current?.duration || 0}
-        onInput={changeInputRange}
-      />
-      <div className="serveral-icons">
-        <div className="volume-wrap width-half">
-          {musicControl.current?.volume === 0 ? (
-            <img className="icon" src="/icons/volume-off.svg" alt="volume-off" onClick={toggleVolume} />
-          ) : (
-            <img className="icon" src="/icons/volume-up.svg" alt="volume-up" onClick={toggleVolume} />
-          )}
-          <div className="progress-wrap width-half">
-            <input
-              className="input-range"
-              name="volume-progress"
-              ref={volumeProgress}
-              type="range"
-              min="0"
-              max="100"
-              onInput={changeVolume}
-            />
+    <>
+    <video src={path} ref={musicControl} autoPlay onTimeUpdate={updateCurrentTime}/> 
+    {musicControl &&
+      <div className="searched-musicplayer">
+        <div className="musicplayer-timer">
+          <span className="current-time">{changeFormatToTime(musicControl.current?.currentTime || 0)}</span>
+          <span className="max-duration">{changeFormatToTime(musicControl.current?.duration || 0)}</span>
+        </div>
+        <input
+          className="input-range"
+          name="musicplayer-progress"
+          ref={musicProgress}
+          type="range"
+          min="0"
+          max={musicControl.current?.duration}
+          onInput={changeInputRange}
+        />
+        <div className="serveral-icons">
+          <div className="volume-wrap width-half">
+            {musicControl.current?.volume === 0 ? (
+              <img className="icon" src="/icons/volume-off.svg" alt="volume-off" onClick={toggleVolume} />
+            ) : (
+              <img className="icon" src="/icons/volume-up.svg" alt="volume-up" onClick={toggleVolume} />
+            )}
+            <div className="progress-wrap width-half">
+              <input
+                className="input-range"
+                name="volume-progress"
+                ref={volumeProgress}
+                type="range"
+                min="0"
+                max="100"
+                onInput={changeVolume}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    }
+    </>
   )
 }
 
